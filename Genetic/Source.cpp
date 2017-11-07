1763a63d35d3e855eaa358dbb0291d2ecea6ee1e
Polina <polliksa@outlook.com>
2017-11-07 16:58:32 +0300

    New schedule representation

--- Genetic/Source.cpp
+++ Genetic/Source.cpp
@@ -9,74 +9,59 @@
 int *jobs;
 int max_time = 0;
 int nProc;
+int nJobs;
 
 using namespace std;
 
 struct job {
 	int id;
 	int length;
-	int cur_time;
 };
 
-struct table {
-	double fitness;
-	int **schedule;
+struct citizen {
+	int id;
+	int fitness;
+	int *schedule;
 };
 
-void shuffle(vector<job> *arr, int n)
-{
-	for (int i = 0; i < n - 1; i++) {
-		int j = i + rand() / (RAND_MAX / (n - i) + 1);
-		job t = (*arr)[j];
-		(*arr)[j] = (*arr)[i];
-		(*arr)[i] = t;
+
+void print_schedule(int *schedule) {
+	for (int i = 0; i < nJobs; i++) {
+		cout << schedule[i] << ' ';
 	}
+	cout << endl;
 }
-table create_element(int nProcs, vector<job> job_order) {
-	auto cur_job = job_order.begin();
 
-	// allocate memory for the schedule — a double array
-	table elem;
-	elem.schedule = new int*[max_time];
-	for (int j = 0; j < max_time; j++) {
-		elem.schedule[j] = new int[nProcs];
-		for (int h = 0; h < nProcs; h++) elem.schedule[j][h] = -1;
+void print_population(vector<citizen> pop) {
+	for (auto i = pop.begin(); i < pop.end(); i++) {
+		cout << "citizen #" << i->id << ": ";
+		print_schedule(i->schedule);
+		cout << "fitness: " << i->fitness << endl << endl;
 	}
 
-	// fill every time tick...
-	for (int t = 0; t < max_time; t++) {
-
-		// ...for every process
-		for (int p = 0; p < nProcs; p++) {
-
-			// is previous job still running?
-			if (elem.schedule[t][p] >= 0) continue;
-			else {
-
-				// nope, start next one; fill in all next ticks for the duration of the job
-				for (int l = 0; l < (*cur_job).length; l++) {
-					elem.schedule[t + l][p] = (*cur_job).id;
-				}
-				cur_job++;
-				if (cur_job == job_order.end()) {
-					return elem;
-				}
-			}
-		}
-	}
 }
-void create_population(vector<table> *pop, int nProcs, int nJobs, vector<job> job_order) {
+
+int random_processor_number() {
+	return rand() % nProc;
+}
+
+int random_citizen_number() {
+	return rand() % POP_SIZE;
+}
+
+void create_population(vector<citizen> *pop) {
 
 	for (int i = 0; i < POP_SIZE; i++) {
-		// for each element in the population job order will be different
-		shuffle(&job_order, nJobs);
-		cout << "creating element " << i << ":\n";
-		for (auto i = job_order.begin(); i < job_order.end(); i++) {
-			cout << (*i).id << ' ' << (*i).length << endl;
+		// Allocate memory for new citizen
+		citizen tmp;
+		tmp.id = i;
+		tmp.schedule = new int[nJobs];
+
+		// Give each job a random processor number
+		for (int j = 0; j < nJobs; j++) {
+			tmp.schedule[j] = random_processor_number();
 		}
-
-		(*pop).push_back(create_element(nProcs, job_order));
-
+		pop->emplace_back(tmp);
 	}
 }
 
@@ -93,33 +78,29 @@ int find_last_column(int ** schedule, int width, int height) {
 	return width;
 }
 
-void print_schedule(int **schedule) {
-	int limit = find_last_column(schedule, max_time, nProc);
-	for (int i = 0; i < nProc; i++) {
-		cout << "PROC " << i << ": ";
-		for (int t = 0; t < limit; t++) {
-			cout << schedule[t][i] << ' ';
-		}
-		cout << endl;
-	}
-}
+void calc_fitness(vector<citizen> *pop, vector<job> job_pool) {
 
-void print_population(vector<table> pop) {
-	for (auto i = pop.begin(); i < pop.end(); i++) {
-		print_schedule(i->schedule);
-		cout << i->fitness << endl;
-	}
-
-}
-
-void calc_fitness(vector<table> *pop, int width, int nProc) {
-
+	int *proc_time = new int[nProc];
 	for (auto i = pop->begin(); i < pop->end(); i++) {
-		i->fitness = find_last_column(i->schedule, max_time, nProc);
+
+		// number of time ticks each processor was busy
+		for (int t = 0; t < nProc; t++) proc_time[t] = 0;
+
+		// add job length
+		for (int j = 0; j < nJobs; j++)
+			proc_time[i->schedule[j]] += job_pool[j].length;
+		
+		// find max processor time
+		int max = 0;
+		for (int i = 0; i < nProc; i++) {
+			if (max < proc_time[i]) max = proc_time[i];
+		}
+
+		i->fitness = max;
 	}
 }
 
-table mate(table a, table b) {
+citizen mate(citizen a, citizen b) {
 
 	return a;
 	// create mask for the genome
@@ -128,10 +109,10 @@ table mate(table a, table b) {
 	bool *mask = new bool[nProc];
 	for (int i = 0; i < nProc; i++) mask[i] = rand() % 2;
 
-	
+
 }
 
-void produce_offspring(vector<table> parents, vector<table> *children) {
+void produce_offspring(vector<citizen> parents, vector<citizen> *children) {
 
 	for (int i = 0; i < parents.size(); i++) {
 		// choose 2 random elements, produce offspring
@@ -141,13 +122,14 @@ void produce_offspring(vector<table> parents, vector<table> *children) {
 	}
 }
 
-void tournament_selection(vector<table> pop, vector<table> *selected) {
+void tournament_selection(vector<citizen> pop, vector<citizen> *selected) {
 
 	for (int i = 0; i < pop.size(); i++) {
 		// choose 2 random elements, compare their fitness
-		int i1 = rand() % pop.size();
-		int i2 = rand() % pop.size();
-		table winner = (pop[i1].fitness < pop[i2].fitness) ? pop[i1] : pop[i2];
+		int i1 = random_citizen_number();
+		int i2 = random_citizen_number();
+		citizen winner = (pop[i1].fitness < pop[i2].fitness) ? pop[i1] : pop[i2];
+		cout << pop[i1].fitness << " vs " << pop[i2].fitness << endl;
 		selected->emplace_back(winner);
 	}
 }
@@ -158,17 +140,17 @@ void mutate() {
 
 int main(void) {
 	nProc = 4;
-	int nJobs = 10;
+	nJobs = 10;
 	srand(time(NULL));
 
 	// PARCER
-	vector<job> job_order;
+	vector<job> job_pool;
 	for (int i = 0; i < nJobs; i++) {
 		job temp;
 		temp.id = i;
 		temp.length = rand() % 10 + 1;
-		job_order.push_back(temp);
-		max_time += job_order[i].length;
+		job_pool.push_back(temp);
+		max_time += job_pool[i].length;
 	}
 
 	/*cout << "printing jobs: \n";
@@ -179,15 +161,17 @@ int main(void) {
 	// ------
 
 
-	vector<table> population;
-	vector<table> selected_parents;
-	vector<table> children;
-	create_population(&population, nProc, nJobs, job_order);
-	calc_fitness(&population, max_time, nProc);
+	vector<citizen> population;
+	vector<citizen> selected_parents;
+	vector<citizen> children;
+	create_population(&population);
+	calc_fitness(&population, job_pool);
 
 	tournament_selection(population, &selected_parents);
+	print_population(selected_parents);
+
 	produce_offspring(selected_parents, &children);
-	calc_fitness(&children, max_time, nProc);
+	calc_fitness(&children, job_pool);
 	//print_population(children);
 	//cout << children.size() << endl;
 
